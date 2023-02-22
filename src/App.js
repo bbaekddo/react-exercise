fb2a3238609e10eee3b8d15bfc130f18e5177025
bbaekddo <bbaekddo100@naver.com>
2023-02-23 00:00:03 +0900

    :construction: toDo 예제

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
 import Button from './Button';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Welcome Back!
-      </header>
-        <Button text={'Continue'} />
-    </div>
-  );
+    const [toDo, setToDo] = useState('');
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === '') {
+            return;
+        }
+        setToDos(currentArray => [toDo, ...toDos])
+        setToDo('');
+        console.log(toDos);
+    };
+
+    return (
+        <div className='App'>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    value={toDo}
+                    type='text'
+                    placeholder='Write your To Do'
+                />
+                <button>Add To Do</button>
+            </form>
+            <Button text={'Continue'} />
+        </div>
+    );
 }
 
 export default App;
