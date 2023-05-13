7b435c809f5c7f56b2fb6622e9f13a70cb0ae83d
bbaekddo <bbaekddo100@naver.com>
2023-05-13 11:25:06 +0900

    :construction: Movie App 만들기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import './App.css';
-import Button from './Button';
-import { useState } from 'react'
+import Home from './routes/Home'
+import Detail from './routes/Detail'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 function App() {
-    const [toDo, setToDo] = useState('');
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === '') {
-            return;
-        }
-        setToDos(currentArray => [toDo, ...toDos])
-        setToDo('');
-        console.log(toDos);
-    };
-
-    return (
-        <div className='App'>
-            <form onSubmit={onSubmit}>
-                <input
-                    onChange={onChange}
-                    value={toDo}
-                    type='text'
-                    placeholder='Write your To Do'
-                />
-                <button>Add To Do</button>
-            </form>
-            {toDos.map((item, index) => (
-                <li>{index} > {item}</li>
-            ))}
-            <Button text={'Continue'} />
-        </div>
-    );
+    return <Router>
+        <Routes>
+            <Route path='/' element={<Home />}>
+            </Route>
+            <Route path='/detail' element={<Detail />}>
+            </Route>
+        </Routes>
+    </Router>;
 }
 
 export default App;
